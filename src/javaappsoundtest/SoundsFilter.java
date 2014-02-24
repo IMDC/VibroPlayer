5160aa4ea1d1246871fbea86d5c3eec6a0a8bd25
Rener Baffa da Silva <renerbaffa@gmail.com>
2014-02-24 13:40:56 -0500

    Adding comments on the Filter
    Adding comments on the file filter.

--- src/javaappsoundtest/SoundsFilter.java
+++ src/javaappsoundtest/SoundsFilter.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javaappsoundtest;
 
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
 /**
+ * Class to filter files. This class filter all the files that are exhibited in the
+ * dialog page.
  *
- * @author imdc
+ * @author Rener Baffa da Silva
  */
 public class SoundsFilter extends FileFilter {
 
@@ -20,25 +16,27 @@ public class SoundsFilter extends FileFilter {
         if ( !file.isDirectory() ) {
             String extension = "";
             
-            int i = file.getAbsolutePath().lastIndexOf ( "." );
-            int p = Math.max ( file.getAbsolutePath().lastIndexOf ( "\\" ), file.getAbsolutePath().lastIndexOf ( "/" ) );
+            /* Split the file path and get the file extension. */
+                int i = file.getAbsolutePath().lastIndexOf ( "." );
+                int p = Math.max ( file.getAbsolutePath().lastIndexOf ( "\\" ), file.getAbsolutePath().lastIndexOf ( "/" ) );
+
+                if ( i > p ) {
+                    extension = file.getAbsolutePath().substring ( i + 1 );
+                }
             
-            if ( i > p ) {
-                extension = file.getAbsolutePath().substring ( i + 1 );
-            }
-            
-            if ( extension != null ) {
-                /* If you want to add more extensions, just change here. */
-                if ( extension.equals ( "mp4" ) || extension.equals ( "wav" ) ) {
-                    return true;
+            /* Check if extension is expected. If so returns true and if not returns false */
+                if ( extension != null ) {
+                    /* If you want to add more extensions, just change the "if" below and add the new extension. */
+                    if ( extension.equals ( "wav" ) ) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
                 }
                 else {
                     return false;
                 }
-            }
-            else {
-                return false;
-            }
         }
         else {
             return true;
@@ -47,7 +45,7 @@ public class SoundsFilter extends FileFilter {
 
     @Override
     public String getDescription() {
-        return "Just sounds";
+        return "WAV files";
     }
     
 }
